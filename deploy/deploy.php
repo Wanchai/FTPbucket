a66296b3666588d872d55ba08aaa0caa17b6dfc4
thomas <thomas.malicet@gmail.com>
2015-10-07 22:21:53 +0200

    Added 'No payload' message to Logs

--- deploy/deploy.php
+++ deploy/deploy.php
@@ -1,11 +1,18 @@
 <?php
 
-if(isset($_POST['payload'])){
-    
+if(isset($_POST['payload']))
+{
     include 'FTPbucket.php';
     
     $go = new FTPbucket();
     $go->init($_POST['payload']);
+}
+else
+{
+    $msg = date("d.m.Y, H:i:s",time()) .': We received nothing from BitBucket ... \n';
+    $log = fopen ("logfile.txt", "a");
+    fputs($log, $msg);
+    fclose($log);
 }
 
 ?>
