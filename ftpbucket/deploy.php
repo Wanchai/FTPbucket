2ddc2790ed649575367a9832e4fe53ac58700290
T. Malicet <thomas.malicet@gmail.com>
2019-08-21 23:28:46 +0200

    Removed BitBucket POST hooks support

--- ftpbucket/deploy.php
+++ ftpbucket/deploy.php
@@ -24,35 +24,27 @@ if ($fp = fopen('logconnection.txt', 'a')) {
     fwrite($fp, 'A PUSH was started at ' . date("d.m.Y, H:i:s", $start_time) . PHP_EOL);
 }
 
-if (isset($_POST['payload'])) {
-    $data = json_decode(stripslashes($_POST['payload']));
 
-    if ($data->canon_url != null) {
-        include 'BBpost.php';
-        $go = new BBpost();
-        $go->init($_POST['payload']);
-    }
+$inputJSON = file_get_contents('php://input');
+$payload = json_decode($inputJSON, TRUE);
+
+if (!isset($payload)) {
+    $msg = date("d.m.Y, H:i:s", time()) . ": PAYLOAD not supported \n";
+    $log = fopen("logpayload.txt", "a");
+    fputs($log, $msg);
+    fclose($log);
+} else if (isset($payload['repository']['html_url']) && substr_count($payload['repository']['html_url'], 'github') === 1) {
+    // GitHub webhook goes here
+    include 'GHjson.php';
+    $go = new GHjson();
+    $go->init($payload);
 } else {
-    $inputJSON = file_get_contents('php://input');
-    $payload = json_decode($inputJSON, TRUE);
-
-    if (!isset($payload)) {
-        $msg = date("d.m.Y, H:i:s", time()) . ": PAYLOAD not supported \n";
-        $log = fopen("logpayload.txt", "a");
-        fputs($log, $msg);
-        fclose($log);
-    } else if (isset($payload['repository']['html_url']) && substr_count($payload['repository']['html_url'], 'github') === 1) {
-        // GitHub webhook goes here
-        include 'GHjson.php';
-        $go = new GHjson();
-        $go->init($payload);
-    } else {
-        // BitBucket webhook goes here
-        include 'BBjson.php';
-        $go = new BBjson();
-        $go->init($payload);
-    }
+    // BitBucket webhook goes here
+    include 'BBjson.php';
+    $go = new BBjson();
+    $go->init($payload);
 }
+
 
 $end_time = microtime(true);
 fwrite($fp, 'The push finally finished at ' . date("d.m.Y, H:i:s", $end_time) . '.' . PHP_EOL);
